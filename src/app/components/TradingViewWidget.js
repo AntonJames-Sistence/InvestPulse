2f254a7f69b3283daf86b9450ea24bea42545eb2
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-03-05 19:45:08 -0500

    Add dollar representation

--- src/app/components/TradingViewWidget.js
+++ src/app/components/TradingViewWidget.js
@@ -1,6 +1,6 @@
 // TradingViewWidget.jsx
 'use client';
-import React, { useEffect, useRef, memo } from 'react';
+import React, { useEffect, useState, useRef, memo } from 'react';
 
 const TradingViewWidget = () => {
   let coinName = "bitcoin";
@@ -14,6 +14,7 @@ const TradingViewWidget = () => {
     try {
       let data = await fetch(priceUrl);
       let jsonData = await data.json();
+      // console.log(jsonData);
       setCoinData(jsonData);
     } catch (error) {
       console.log(error)
@@ -47,7 +48,7 @@ const TradingViewWidget = () => {
         }`;
       container.current.appendChild(script);
       // handleFetchPrices();
-
+      
       // "hide_top_toolbar": true,
 
       // Cleanup function to remove the script when the component unmounts
@@ -59,14 +60,36 @@ const TradingViewWidget = () => {
     },
     []
   );
+  console.log(coinData)
 
   return (
     <div className="flex flex-col">
-      <div className="h-40 bg-white">
+      <div className="h-40 bg-white rounded-t-lg">
         {coinData ? (
-          <div>loading...</div>
+          <div>
+            <div className='text-black font-xl'>{`${coinData[0].usd}`}</div>
+            <div className='text-black font-xl'>$46,953.04</div>
+            {/* <div className='text-black'>loading...</div> */}
+          </div>
         ):(
-          <div>loading...</div>
+          <>
+          <div></div>
+
+          <div>
+            <div></div>
+          </div>
+          <div className='flex flex-row my-2'>
+            <div className='text-black text-3xl font-semibold pl-4 self-center'>{`$46,953.04`}</div>
+
+            <div class="flex flex-row bg-green-100 bg-opacity-50 rounded-md px-6 py-1 mx-4 text-green-600 self-center relative">
+              <div className="triangle-green self-center border-red"></div>
+              <div>2.51%</div>
+            </div>
+
+            <div className="text-gray-500 text-sm self-center">{`(24H)`}</div>
+          </div>
+          <div className='text-black text-lg pl-5'>{`\u20B9 ${56,97,177 }`}</div>
+          </>
         )}
       </div>
       <div className="tradingview-widget-container" ref={container}>
@@ -74,5 +97,12 @@ const TradingViewWidget = () => {
     </div>
   );
 }
+
+// "bitcoin": {
+//   "inr": 5697177,
+//   "inr_24h_change": 3.6596920153414336,
+//   "usd": 68726,
+//   "usd_24h_change": 3.6767559459431545
+// }
 
 export default memo(TradingViewWidget);
