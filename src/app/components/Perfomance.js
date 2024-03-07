c477278dd39e660a75b58fc92030c422fc104c91
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-03-07 14:28:32 -0500

    Add table representation to Perfomance component

--- src/app/components/Perfomance.js
+++ src/app/components/Perfomance.js
@@ -1,6 +1,7 @@
 'use client';
 import ReusableInfoBlock from "./ReusableInfoBlock";
 import React, { useState } from "react";
+import { fundamentals } from "../data/fundamentals";
 
 const Perfomance = () => {
     const [showFundamentalsTip, setShowFundamentalsTip] = useState(false);
@@ -25,15 +26,57 @@ const Perfomance = () => {
                         {showFundamentalsTip && (
                             <div className="absolute bg-gray-100 p-2 rounded-lg text-sm text-gray-700 w-60 z-10">
                                 <ul className="list-disc pl-4">
-                                <li>Price</li>
-                                <li>Market Capitalization</li>
-                                <li>Trading Volume</li>
-                                <li>Hash Rate</li>
-                                <li>Transaction Speed</li>
+                                {fundamentals.map((event, index) => 
+                                        <li key={index}>{event.title}</li>
+                                )}
                                 </ul>
                             </div>
                         )}
                     </div>
+                </div>
+
+                <div className="grid grid-cols-2 gap-x-20">
+                    <div>
+                        {fundamentals.slice(0, 5).map((event, index) => 
+                            <div>
+                                <div key={index} className="flex justify-between text-sm">
+                                    <span className="text-gray-700">{event.title}</span>
+                                    <span className="">{event.numbers}</span>
+                                </div>
+                                <hr className="border-gray-400 my-4" />
+                            </div>
+                        )}
+                    </div>
+                    <div>
+                        {fundamentals.slice(5).map((event, index) => 
+                            <div>
+                                <div key={index} className="flex justify-between text-sm">
+                                    <span className="text-gray-500">{event.title}</span>
+                                    <span>{event.numbers}</span>
+                                </div>
+                                <hr className="border-gray-400 my-4" />
+                            </div>
+                        )}
+                        <div className="flex flex justify-between text-sm w-full max-h-[20px]">
+                            <span className="text-gray-500 self-center">All-Time High</span>
+                            <div className="flex flex-col relative -top-[10px]">
+                                <span className="self-end">$69,044.77<p className="text-red-500 inline-block ml-1">-75.6%</p></span>
+                                <span className="text-xs">Nov 10, 2021 (about 1 year)</span>
+                            </div>
+                        </div>
+                        <hr className="border-gray-400 my-4" />
+
+                        <div className="flex flex justify-between text-sm w-full max-h-[20px]">
+                            <span className="text-gray-500 self-center">All-Time Low</span>
+                            <div className="flex flex-col relative -top-[10px]">
+                                <span className="self-end">$67.81<p className="text-green-500 inline-block ml-1">24749.1%</p></span>
+                                <span className="text-xs">Jul 06, 2013 (over 9 years)</span>
+                            </div>
+                        </div>
+                        <hr className="border-gray-400 my-4" />
+                    </div>
+                    
+                    
                 </div>
             </div>
         </ReusableInfoBlock>
