9a8f6871006cc6f501df5383f805f90f1d8f1b79
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-03-07 11:37:35 -0500

    Add dynamic functionality to circle in Sentiment component

--- src/app/components/Sentiment.js
+++ src/app/components/Sentiment.js
@@ -1,11 +1,31 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReusableInfoBlock from "./ReusableInfoBlock";
 
 
 const Sentiment = () => {
     const [showKeyEventsTip, setShowKeyEventsTip] = useState(false);
     const [showAnalystEstimatesTip, setShowAnalistEstimatesTip] = useState(false);
+    const [maxPercentage, setMaxPercentage] = useState(0);
+    const [maxColor, setMaxColor] = useState('');
+
+    const buyPercentage = 76;
+    const holdPercentage = 8;
+    const sellPercentage = 16;
+
+    // Dynamic change of color and number inside the circle graph
+    useEffect(() => {
+        const max = Math.max(buyPercentage, holdPercentage, sellPercentage);
+        setMaxPercentage(max);
+
+        if (max === buyPercentage) {
+            setMaxColor('bg-green-100 text-green-600');
+        } else if (max === holdPercentage) {
+            setMaxColor('bg-gray-100 text-gray-600');
+        } else {
+            setMaxColor('bg-red-100 text-red-600');
+        }
+    }, [buyPercentage, holdPercentage, sellPercentage]);
 
     return (
         <ReusableInfoBlock title="Sentiment">
@@ -59,6 +79,32 @@ const Sentiment = () => {
                                 </ul>
                             </div>
                         )}
+                    </div>
+                </div>
+
+                <div className='flex'>
+                    <div className={`rounded-full ${maxColor} h-28 w-28 flex justify-center items-center mr-10`}>
+                        <p className='font-semibold text-4xl'>{maxPercentage}</p><p className='text-sm'>%</p>
+                    </div>
+                    
+                    <div className="flex flex-col h-12 w-52 text-gray-600 w-[80%]">
+                        <div className='flex my-2'>
+                            <p className='w-16'>Buy</p>
+                            <div className="bg-green-600 rounded-sm h-2 self-center mr-2" style={{ width: `${buyPercentage > 70 ? buyPercentage - 30 : buyPercentage}%` }}></div>
+                            <p className='text-sm self-center'>{buyPercentage}%</p>
+                        </div>
+
+                        <div className='flex mb-2'>
+                            <p className='w-16'>Hold</p>
+                            <div className="bg-gray-300 rounded-sm h-2 self-center mr-2" style={{ width: `${holdPercentage > 70 ? holdPercentage - 30 : holdPercentage}%` }}></div>
+                            <p className='text-sm self-center'>{holdPercentage}%</p>
+                        </div>
+
+                        <div className='flex'>
+                            <p className='w-16'>Sell</p>
+                            <div className="bg-red-500 rounded-sm h-2 self-center mr-2" style={{ width: `${sellPercentage > 70 ? sellPercentage - 30 : sellPercentage}%` }}></div>
+                            <p className='text-sm self-center'>{sellPercentage}%</p>
+                        </div>
                     </div>
                 </div>
             </div>
