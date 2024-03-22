1a6f1c4a6e06636b52de26cee2e308676920c83d
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-03-22 13:49:35 -0400

    Fix bug with favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <link rel="icon" href="/favicon.png" sizes="any" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
     </html>
   );
 }
