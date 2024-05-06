9a4c4ed542223417c26c60b1f4969947a658f769
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-05-06 12:30:53 -0400

    Transfer ReusableTile and Tokenomics components to TSX

--- src/app/components/ReusableTile.tsx
+++ src/app/components/ReusableTile.tsx
@@ -1,4 +1,11 @@
-const ReusableTile = ({ title, children }) => {
+import React, { ReactNode } from 'react';
+
+interface ReusableTileProps {
+  title: string,
+  children: ReactNode;
+}
+
+const ReusableTile: React.FC<ReusableTileProps> = ({ title, children }) => {
   return (
     <div className="flex flex-col bg-white rounded-lg p-2 lg:p-8 mb-4 lg:mb-8">
       <div className="font-semibold text-2xl mb-4 lg:mb-8">{title}</div>
