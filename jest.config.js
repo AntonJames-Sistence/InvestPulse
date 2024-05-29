5298f23204592701e723835701635f7ed0e66871
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-05-28 23:10:34 -0400

    Add jest for testing

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+};
+  
