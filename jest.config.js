64bfa64ae456838fd06fdbad1ab56d7e070f955f
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-05-29 11:45:42 -0400

    Remove jest library and corresponding test files

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
-};
