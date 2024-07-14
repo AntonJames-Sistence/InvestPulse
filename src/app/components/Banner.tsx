ea88b5e5f44c098153b61ec22eca1cde3a2288c6
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-07-13 22:08:14 -0400

    Improve perfomance by adding next/image as sub to regular img tags

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,24 +1,8 @@
-import { FaArrowRightLong } from "react-icons/fa6";
 
-const Banner = () => {
 
-    return (
-        <div className="p-10 bg-[#1f54f4] rounded-xl flex flex-col items-center text-center text-white mb-4">
-            <p className="text-xl font-semibold mb-4 max-w-[300px]">Get Started with KoinY for FREE</p>
-            <p className="text-xs">
-                With our range of features that you can equip for free, KoinY 
-                allows you to be more educated and aware or your lax reports.
-            </p>
-            <img className="w-40 h-40 mt-4" src="./placeholder3.png" />
-            
-            <a href="https://accounts.coinbase.com/signup" target="_blanc">
-                <button className="flex bg-white text-black rounded-xl py-3 px-4 mt-4 font-semibold text-sm hover:bg-sky-400 duration-200 easy-in-out"> 
-                    <p>Get Started for FREE</p>
-                    <FaArrowRightLong className="self-center ml-2" />
-                </button>
-            </a>
-        </div>
-    )
-}
+// const Banner = () => {
 
-export default Banner;
+    
+// }
+
+// export default Banner;
--- src/app/components/Perfomance/TradingViewWidget.tsx
+++ src/app/components/Perfomance/TradingViewWidget.tsx
@@ -1,44 +1,45 @@
-'use client';
+"use client";
 
-import React, { useEffect, useRef } from 'react';
-import { formatAsUSD } from '../../utils/formatAsUsd';
+import React, { useEffect, useRef } from "react";
+import { formatAsUSD } from "../../utils/formatAsUsd";
+import Image from "next/image";
 
 interface TradingViewWidgetProps {
-  coinData : {
-    symbol: string,
-    name: string,
-    description: string,
-    homepage: string,
-    image: string,
-    market_cap_rank: number,
-    current_price: number,
-    ath: number,
-    ath_change_percentage: number,
-    ath_date: Date,
-    atl: number,
-    atl_change_percentage: number,
-    atl_date: Date,
-    market_cap: number,
-    total_volume: number,
-    high_24h: number,
-    low_24h: number,
-    price_change_24h: number,
-    price_change_percentage_24h: number,
-    price_change_percentage_7d: number,
-    price_change_percentage_1y: number,
+  coinData: {
+    symbol: string;
+    name: string;
+    description: string;
+    homepage: string;
+    image: string;
+    market_cap_rank: number;
+    current_price: number;
+    ath: number;
+    ath_change_percentage: number;
+    ath_date: Date;
+    atl: number;
+    atl_change_percentage: number;
+    atl_date: Date;
+    market_cap: number;
+    total_volume: number;
+    high_24h: number;
+    low_24h: number;
+    price_change_24h: number;
+    price_change_percentage_24h: number;
+    price_change_percentage_7d: number;
+    price_change_percentage_1y: number;
     last_updated: Date;
-  }
+  };
 }
 
 const TradingViewWidget: React.FC<TradingViewWidgetProps> = ({ coinData }) => {
   const container = useRef<HTMLDivElement>(null);
-  const isMobile = typeof window !== 'undefined' && window.innerWidth <= 768;
+  const isMobile = typeof window !== "undefined" && window.innerWidth <= 768;
 
   useEffect(() => {
-    if (coinData){
+    if (coinData) {
       generateTradingViewWidget(coinData.symbol, isMobile);
     }
-  }, [coinData])
+  }, [coinData]);
 
   const generateTradingViewWidget = (coinSymbol: string, isMobile: boolean) => {
     if (!container.current) return;
@@ -48,7 +49,8 @@ const TradingViewWidget: React.FC<TradingViewWidgetProps> = ({ coinData }) => {
     }
 
     const script = document.createElement("script");
-    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
     script.type = "text/javascript";
     script.async = true;
     script.innerHTML = `
@@ -72,47 +74,73 @@ const TradingViewWidget: React.FC<TradingViewWidgetProps> = ({ coinData }) => {
         "support_host": "https://www.tradingview.com"
       }`;
 
-      container.current.appendChild(script);
+    container.current.appendChild(script);
   };
-  
+
   return (
-      <div className="flex flex-col mb-4 lg:mb-8">
-        <div className="h-auto flex flex-col bg-white rounded-lg">
-          
-            <div className='flex flex-col justify-between mx-5'>
-              <div className='flex flex-row mt-7 mb-10'>
-                <img src={coinData.image} alt={`${coinData.name} logo`} className='h-10 w-10 bg-white'></img>
-                <div className='text-black text-2xl font-semibold self-center mx-2'>{coinData.name}</div>
-                <div className='text-gray-500 self-center font-semibold mr-2'>{coinData.symbol}</div>
-
-                <div className='flex flex-row bg-gray-600 bg-opacity-70 rounded-lg p-2 ml-10 text-white self-center'>
-                  {`Rank #${coinData.market_cap_rank}`}
-                </div>
-              </div>
-
-              <div className='flex flex-row'>
-                <div className='text-black text-3xl font-semibold self-center'>{formatAsUSD(coinData?.current_price ?? 0)}</div>
-
-                <div className={`flex flex-row bg-${coinData.price_change_percentage_24h < 0 ? 'red' : 'green'}-100 bg-opacity-50 rounded-md px-4 py-1 ml-6 mr-2 text-${coinData.price_change_percentage_24h < 0 ? 'red' : 'green'}-600 self-center`}>
-                  <div className={`triangle-${coinData.price_change_percentage_24h < 0 ? 'red' : 'green'} self-center border-${coinData.price_change_percentage_24h < 0 ? 'red' : 'green'}`}></div>
-                  <div>{`${Math.abs(coinData.price_change_percentage_24h)}%`}</div>
-                </div>
-
-                <div className="text-gray-500 text-sm self-center">{`(24H)`}</div>
-              </div>
-
-              <hr className='my-5 border-gray-400' />
-
-              <div className='mb-10 font-semibold text'>{`${coinData.name} Price Chart (USD)`}</div>
+    <div className="flex flex-col mb-4 lg:mb-8">
+      <div className="h-auto flex flex-col bg-white rounded-lg">
+        <div className="flex flex-col justify-between mx-5">
+          <div className="flex flex-row mt-7 mb-10">
+            <Image
+              className="h-10 w-10 bg-white"
+              src={coinData.image}
+              alt={`${coinData.name} logo`}
+              height={100}
+              width={100}
+              priority={true}
+            />
+            <div className="text-black text-2xl font-semibold self-center mx-2">
+              {coinData.name}
             </div>
-          
-          <div className='mx-5 mb-5 z-10'>
-            <div className="tradingview-widget-container self-center" ref={container}></div>
+            <div className="text-gray-500 self-center font-semibold mr-2">
+              {coinData.symbol}
+            </div>
+
+            <div className="flex flex-row bg-gray-600 bg-opacity-70 rounded-lg p-2 ml-10 text-white self-center">
+              {`Rank #${coinData.market_cap_rank}`}
+            </div>
           </div>
 
-        </div>                                                                 
+          <div className="flex flex-row">
+            <div className="text-black text-3xl font-semibold self-center">
+              {formatAsUSD(coinData?.current_price ?? 0)}
+            </div>
+
+            <div
+              className={`flex flex-row bg-${
+                coinData.price_change_percentage_24h < 0 ? "red" : "green"
+              }-100 bg-opacity-50 rounded-md px-4 py-1 ml-6 mr-2 text-${
+                coinData.price_change_percentage_24h < 0 ? "red" : "green"
+              }-600 self-center`}
+            >
+              <div
+                className={`triangle-${
+                  coinData.price_change_percentage_24h < 0 ? "red" : "green"
+                } self-center border-${
+                  coinData.price_change_percentage_24h < 0 ? "red" : "green"
+                }`}
+              ></div>
+              <div>{`${Math.abs(coinData.price_change_percentage_24h)}%`}</div>
+            </div>
+
+            <div className="text-gray-500 text-sm self-center">{`(24H)`}</div>
+          </div>
+
+          <hr className="my-5 border-gray-400" />
+
+          <div className="mb-10 font-semibold text">{`${coinData.name} Price Chart (USD)`}</div>
+        </div>
+
+        <div className="mx-5 mb-5 z-10">
+          <div
+            className="tradingview-widget-container self-center"
+            ref={container}
+          ></div>
+        </div>
       </div>
+    </div>
   );
-}
+};
 
 export default TradingViewWidget;
