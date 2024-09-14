07efae9cdaf5a72034e013a43f953e1bb82d30dc
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-09-14 11:13:23 -0400

    Fix generateView, remove unused dep

--- src/app/components/Perfomance/GenerateView.tsx
+++ src/app/components/Perfomance/GenerateView.tsx
@@ -8,71 +8,55 @@ interface GenerateViewProps {
 }
 
 const GenerateView: React.FC<GenerateViewProps> = ({ stockSymbol }) => {
-  const containerRef = useRef<HTMLDivElement>(null);
+  const container = useRef<HTMLDivElement>(null);
   const isMobile = typeof window !== 'undefined' && window.innerWidth <= 768;
 
+  const generateTradingViewWidget = (symbol: string, isMobile: boolean) => {
+    if (!container.current) return;
+
+    // Clear existing children
+    while (container.current.firstChild) {
+      container.current.removeChild(container.current.firstChild);
+    }
+
+    const script = document.createElement('script');
+    script.src =
+      'https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js';
+    script.type = 'text/javascript';
+    script.async = true;
+    script.innerHTML = `
+      {
+        "width": "100%",
+        "height": "${isMobile ? 300 : 410}",
+        "symbol": "${symbol}",
+        "hide_legend": true,
+        "interval": "D",
+        "timezone": "Etc/UTC",
+        "theme": "light",
+        "style": "3",
+        "locale": "en",
+        "enable_publishing": false,
+        "backgroundColor": "rgba(255, 255, 255, 1)",
+        "gridColor": "rgba(0, 0, 0, 0.06)",
+        "save_image": false,
+        "calendar": false,
+        "hide_volume": true,
+        "hide_top_toolbar": true,
+        "support_host": "https://www.tradingview.com"
+      }`;
+
+    // script.onload = () => setLoading(false);
+    container.current.appendChild(script);
+  };
+
   useEffect(() => {
-    const scriptId = 'tradingview-widget-script';
-
-    const loadWidget = () => {
-      if (containerRef.current) {
-        // Clear the container
-        containerRef.current.innerHTML = '';
-
-        // Create the widget
-        new window.TradingView.widget({
-          width: '100%',
-          height: isMobile ? 300 : 410,
-          symbol: stockSymbol,
-          hide_legend: true,
-          interval: 'D',
-          timezone: 'Etc/UTC',
-          theme: 'light',
-          style: '3',
-          locale: 'en',
-          enable_publishing: false,
-          backgroundColor: 'rgba(255, 255, 255, 1)',
-          gridColor: 'rgba(0, 0, 0, 0.06)',
-          save_image: false,
-          calendar: false,
-          hide_volume: true,
-          hide_top_toolbar: true,
-          container_id: containerRef.current.id,
-        });
-      }
-    };
-
-    const loadTradingViewScript = () => {
-      if (typeof window.TradingView === 'undefined') {
-        const script = document.createElement('script');
-        script.id = scriptId;
-        script.type = 'text/javascript';
-        script.src = 'https://s3.tradingview.com/tv.js';
-        script.async = true;
-        script.onload = () => {
-          loadWidget();
-        };
-        document.head.appendChild(script);
-      } else {
-        loadWidget();
-      }
-    };
-
-    loadTradingViewScript();
-
-    return () => {
-      // Clean up the widget when the component unmounts
-      if (containerRef.current) {
-        containerRef.current.innerHTML = '';
-      }
-    };
+    generateTradingViewWidget(stockSymbol, isMobile);
   }, [stockSymbol, isMobile]);
 
   return (
     <Box
       className="tradingview-widget-container self-center"
-      ref={containerRef}
-      id="tradingview-widget"
+      ref={container}
     ></Box>
   );
 };
